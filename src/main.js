cc8e127b2e958d61bbdbc05265d6b60918ab17c6
Faan Veldhuijsen <Faan Veldhuijsen>
2016-11-14 17:39:54 +0100

    Code cleanup

--- src/main.js
+++ src/main.js
@@ -1,42 +1,60 @@
-function getDoctypeCommand(editor) {
-    var document    = editor.document,
-        start       = pos.ch,
-        end         = pos.ch,
-        line        = document.getLine(pos.line),
-        pos         = editor.getCursorPos(),
-        command     = "";
+define(function (require, exports, module) {
+    "use strict";
 
-    while (start > 0 && (/\S/).test(line.charAt(start - 1))) {
-        --start;
+    /*
+     *   Brackets modules
+     */
+    var KeyEvent = brackets.getModule("utils/KeyEvent"),
+        EditorManager = brackets.getModule("editor/EditorManager");
+
+    var allowedExtensions = [
+        ".php",
+        ".html"
+    ];
+    var doctypeText = "<!DOCTYPE html>\n<html>\n<head>\n</head>\n<body>\n</body>\n</html>";
+    var command = "doctype";
+
+    function getCommand(editor) {
+        var document    = editor.document,
+            pos         = editor.getCursorPos(),
+            line        = document.getLine(pos.line),
+            start       = pos.ch,
+            end         = pos.ch,
+            cmnd        = false;
+
+        while (start > 0 && (/\S/).test(line.charAt(start - 1))) {
+            --start;
+        }
+
+        cmnd = document.getRange({line: pos.line, ch: start}, {line: pos.line, ch: end});
+
+        if (cmnd.match(/doctype/)) {
+            cmnd = cmnd.substring(cmnd.match(/doctype/).index);
+        }
+
+        return cmnd === command ? true : false;
     }
 
-    command = document.getRange({line: pos.line, ch: start}, {line: pos.line, ch: end});
+    function handleKeyEvent(jqEvent, editor, event) {
+        var cmnd = false,
+            text = "",
+            start = 0,
+            end = 0,
+            codemirror = null,
+            i = 0,
+            filename = editor.document.file._name;
 
-    if (command.match(/doctype/)) {
-        command = command.substring(command.match(/doctype/).index);
-    }
-
-    return ((command.split("_")[0] === "doctype") ? command : "");
-}
-
-function handleKeyEvent(jqEvent, editor, event) {
-    var command     = "",
-        text        = "",
-        start       = 0,
-        end         = 0,
-        codemirror  = null,
-        i           = 0,
-        filename    = editor.document.file._name;
-
-    if ((event.type === "keydown") && (event.keyCode === KeyEvent.DOM_VK_TAB) && allowedExtensions.indexOf(filename.substr(filename.lastIndexOf("."))) != -1) {
-        command = getDoctypeCommand(editor);
-        if (command) {
-            text    = doctypeText;
-            end     = editor.getCursorPos();
-            start   = {line: end.line, ch: end.ch - command.length};
-            editor.document.replaceRange(text, start, end);
-
-            // Fix the line indentation
+        if ((event.type === "keydown") && (event.keyCode === KeyEvent.DOM_VK_TAB) && allowedExtensions.indexOf(filename.substr(filename.lastIndexOf("."))) != -1) {
+            cmnd = getCommand(editor);
+            if (cmnd) {
+                text = doctypeText;
+                end = editor.getCursorPos();
+                start = {
+                    line: end.line,
+                    ch: end.ch - command.length
+                };
+                editor.document.replaceRange(text, start, end);
+            }
             codemirror = editor._codeMirror;
             if (codemirror) {
                 end = editor.getCursorPos();
@@ -48,7 +66,6 @@ function handleKeyEvent(jqEvent, editor, event) {
             event.preventDefault();
         }
     }
-}
 
     function editorListener(event, newEditor, oldEditor) {
         if (newEditor) {
@@ -59,22 +76,6 @@ function handleKeyEvent(jqEvent, editor, event) {
             $(oldEditor).off("keyEvent", handleKeyEvent);
         }
     }
-define(function (require, exports, module) {
-    "use strict";
-
-    /*
-    *   Brackets modules
-    */
-    var KeyEvent        = brackets.getModule("utils/KeyEvent"),
-        EditorManager   = brackets.getModule("editor/EditorManager");
-
-    var allowedExtensions = [
-        ".php",
-        ".html"
-    ];
-    var doctypeText = "<!DOCTYPE html>\n<html>\n<head>\n</head>\n<body>\n</body>\n</html>";
-
-
 
     $(EditorManager).on("activeEditorChange", editorListener);
     $(EditorManager.getCurrentFullEditor()).on("keyEvent", handleKeyEvent);
